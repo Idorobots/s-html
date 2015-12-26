8c276ce843687a6d24e69fa639f788bc331c5ea7
Kajetan Rzepecki <kajetan.rzepecki+github@gmail.com>
2015-12-26 22:33:33 +0100

    Added some convenience functions for tag creation.

--- src/s_html/tags.clj
+++ src/s_html/tags.clj
@@ -25,3 +25,28 @@
 
   ([name]
    (tag name)))
+
+(defn make-tag [constructor name]
+  `(def ~name (partial ~constructor '~name)))
+
+(defmacro deftag [name]
+  (make-tag tag name))
+
+(defmacro defemptytag [name]
+  (make-tag empty-tag name))
+
+(defmacro deftags [tags]
+  `(do ~@(map (partial make-tag tag) tags)))
+
+(defmacro defemptytags [tags]
+  `(do ~@(map (partial make-tag empty-tag) tags)))
+
+(deftags
+  [a abbr acronym address applet b bdo big blockquote body body button caption center cite code
+   colgroup dd del dfn dir div dl dt em fieldset font form frame frameset h1 h2 h3 h4 h5 h6 head
+   html i iframe ins kbd label legend li menu noframe noscript object ol optgroup option p pre q
+   s samp script select small span strike strong style sub sup table tbody td textarea tfoot th
+   thead title tr tt u var])
+
+(defemptytags
+  [area base basefont br col hr img input isindex link meta param])
