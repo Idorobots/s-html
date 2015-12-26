9f27e9f9c8aeda840c5b0c4aa73bcd6dc8985e73
Kajetan Rzepecki <kajetan.rzepecki+github@gmail.com>
2015-12-26 22:48:41 +0100

    Added some tests for the printer.

---
+++ src/s_html/print.clj
@@ -0,0 +1,5 @@
+(ns s-html.print)
+
+(defn html->str [html]
+  ;; TODO
+  )
---
+++ test/s_html/print_test.clj
@@ -0,0 +1,17 @@
+(ns s-html.print-test
+  (:require [clojure.test :refer :all]
+            [s-html.tags :as t]
+            [s-html.print :refer [html->str]]))
+
+(deftest can-stringify-html
+  (is (= (html->str nil) ""))
+  (is (= (html->str (list)) ""))
+  (is (= (html->str (t/html)) "<html></html>"))
+  (is (= (html->str (t/doctype :html)) "<!DOCTYPE html>"))
+  (is (= (html->str (list (t/doctype :html)
+                          (t/html)))
+         "<!DOCTYPE html><html></html>"))
+  (is (= (html->str (t/div {:class :container}))
+         "<div class=\"container\"></div>"))
+  (is (= (html->str (t/div (t/a {:href "#"})))
+         "<div><a href=\"#\"></a></div>")))
