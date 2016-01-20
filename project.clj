1b0566eaf110ca8fab883a72e0dd91960c5b8d33
Kajetan Rzepecki <kajetan.rzepecki+github@gmail.com>
2016-01-20 15:42:09 +0100

    Added Codox to the deps.

--- project.clj
+++ project.clj
@@ -6,4 +6,6 @@
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :plugins [[jonase/eastwood "0.1.5"]
             [lein-ancient "0.5.4"]
-            [lein-cloverage "1.0.2"]])
+            [lein-cloverage "1.0.2"]
+            [lein-codox "0.9.1"]]
+  :codox {:source-uri "https://github.com/Idorobots/s-html/blob/{version}/{filepath}#L{line}"})
