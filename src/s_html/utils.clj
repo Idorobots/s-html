e3098d16c64bbd58592c6fa33aaff2a422288e2f
Kajetan Rzepecki <kajetan.rzepecki+github@gmail.com>
2016-02-08 20:58:26 +0100

    Made attribute accessor functions public.

--- src/s_html/utils.clj
+++ src/s_html/utils.clj
@@ -13,9 +13,11 @@
       seq
       sort))
 
-(defn- add-attr [{:keys [attrs] :as tag} attribute value]
+(defn add-attr
+  "Adds all `values` to the `tag`'s `attribute`."
+  [tag attribute & values]
   (update-in tag [:attrs attribute]
-             #(union-values % value)))
+             (partial union-values values)))
 
 (defn- diff-values [a b]
   (-> (difference (settify a)
@@ -28,30 +30,34 @@
     (assoc m k (dissoc-in (m k) ks))
     (dissoc m k)))
 
-(defn- remove-attr [tag attribute value]
+(defn remove-attr
+  "Removes all `values` from `tag`'s `attribute`."
+  [tag attribute & values]
   (let [a (get-in tag [:attrs attribute])
-        d (diff-values a value)]
+        d (diff-values a values)]
     (if (empty? d)
       (dissoc-in tag [:attrs attribute])
       (assoc-in tag [:attrs attribute] d))))
 
-(defn- has-attr [tag attribute value]
+(defn has-attr
+  "Checks wether `tag`'s `attribute` contains all `values`."
+  [tag attribute & values]
   (let [a (get-in tag [:attrs attribute])
-        sv (settify value)]
+        sv (settify values)]
     (= (intersection (settify a) sv)
        sv)))
 
 (defn add-class
   "Adds all classes in `cs` to the `tag`'s `:class` attribute."
   [tag & cs]
-  (add-attr tag :class cs))
+  (apply add-attr tag :class cs))
 
 (defn remove-class
   "Removes all classes in `cs` from `tag`'s `:class` attribute."
   [tag & cs]
-  (remove-attr tag :class cs))
+  (apply remove-attr tag :class cs))
 
 (defn has-class
   "Checks wether `tag`'s `:class` attribute contains all classes in `cs`."
   [tag & cs]
-  (has-attr tag :class cs))
+  (apply has-attr tag :class cs))
