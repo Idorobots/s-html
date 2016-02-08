7ab523e972f48ac4630fc7e31789453a7fd7e2f8
Kajetan Rzepecki <kajetan.rzepecki+github@gmail.com>
2016-02-08 20:10:31 +0100

    Added a util for class removal.

--- src/s_html/utils.clj
+++ src/s_html/utils.clj
@@ -13,7 +13,33 @@
   (update-in tag [:attrs attribute]
              #(join-values % value)))
 
+(defn- dissoc-in [m [k & ks]]
+  (if ks
+    (assoc m k (dissoc-in (m k) ks))
+    (dissoc m k)))
+
+(defn- remove-attr [tag attribute value]
+  (let [a (get-in tag [:attrs attribute])]
+    (cond (= value a)
+          (dissoc-in tag [:attrs attribute])
+
+          (sequential? a)
+          (let [f (filter #(not= value %) a)]
+            (if (empty? f)
+              (dissoc-in tag [:attrs attribute])
+              (assoc-in tag [:attrs attribute] f)))
+
+          :else
+          tag)))
+
 (defn add-class
   "Adds all classes in `cs` to the `tag`'s `:class` attribute."
   [tag & cs]
   (add-attr tag :class cs))
+
+(defn remove-class
+  "Removes all classes in `cs` from `tag`'s `:class` attribute."
+  [tag & cs]
+  (reduce #(remove-attr %1 :class %2)
+          tag
+          cs))
--- test/s_html/utils_test.clj
+++ test/s_html/utils_test.clj
@@ -1,7 +1,7 @@
 (ns s-html.utils-test
   (:require [clojure.test :refer :all]
             [s-html.tags :refer [i]]
-            [s-html.utils :refer [add-class]]))
+            [s-html.utils :refer [add-class remove-class]]))
 
 (deftest can-add-a-class
   (is (= (add-class (i) :hurr)
@@ -14,3 +14,17 @@
          (i {:class [:herp :derp :hurr]})))
   (is (= (add-class (i {:class [:herp :derp]}) :hurr :durr)
          (i {:class '(:herp :derp :hurr :durr)}))))
+
+(deftest can-remove-a-class
+  (is (= (remove-class (i) :hurr)
+         (i)))
+  (is (= (remove-class (i) :hurr :durr)
+         (i)))
+  (is (= (remove-class (i {:class :hurr}) :hurr)
+         (i)))
+  (is (= (remove-class (i {:class [:hurr :durr]}) :hurr :durr)
+         (i)))
+  (is (= (remove-class (i {:class [:hurr :durr]}) :hurr)
+         (i {:class '(:durr)})))
+  (is (= (remove-class (i {:class [:hurr :durr :derp]}) :herp :derp)
+         (i {:class '(:hurr :durr)}))))
