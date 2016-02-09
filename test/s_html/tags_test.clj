7d0285429bc705958e16eb2d23eb62c388ded8c5
Kajetan Rzepecki <kajetan.rzepecki+github@gmail.com>
2016-02-09 19:44:44 +0100

    Added RFC4287 Atom tags.

--- test/s_html/tags_test.clj
+++ test/s_html/tags_test.clj
@@ -1,6 +1,8 @@
 (ns s-html.tags-test
   (:require [clojure.test :refer :all]
-            [s-html.tags :as t]))
+            [s-html.tags :as t]
+            [s-html.tags.atom :as atom]
+            [s-html.utils :refer [attr]]))
 
 (deftest subelements-arent-attributes
   (let [inner (t/a {:href "#"})
@@ -13,3 +15,9 @@
          (:contents (t/i {:class :class}))))
   (is (= (:contents (t/i "foo"))
          (:contents (t/i {:class :class} "foo")))))
+
+(deftest atom-feed-is-well-formed
+  (is (attr (atom/feed) :xmlns))
+  (is (attr (atom/feed {:type "text"}) :xmlns))
+  (is (attr (second (atom/feed*)) :xmlns))
+  (is (t/xml? (first (atom/feed*)))))
