bcde2d12007b237ca8576dbb6c19ceb0aae97ed5
Kajetan Rzepecki <kajetan.rzepecki+github@gmail.com>
2016-02-09 20:30:02 +0100

    Added RSS 2.0 tags.

--- test/s_html/tags_test.clj
+++ test/s_html/tags_test.clj
@@ -2,6 +2,7 @@
   (:require [clojure.test :refer :all]
             [s-html.tags :as t]
             [s-html.tags.atom :as atom]
+            [s-html.tags.rss :as rss]
             [s-html.utils :refer [attr]]))
 
 (deftest subelements-arent-attributes
@@ -21,3 +22,9 @@
   (is (attr (atom/feed {:type "text"}) :xmlns))
   (is (attr (second (atom/feed*)) :xmlns))
   (is (t/xml? (first (atom/feed*)))))
+
+(deftest rss-feed-is-well-formed
+  (is (attr (rss/rss) :version))
+  (is (attr (rss/rss {:xmlns "..."}) :version))
+  (is (attr (second (rss/rss*)) :version))
+  (is (t/xml? (first (rss/rss*)))))
